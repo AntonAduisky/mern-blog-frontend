61ebd8a8e8163d240e48ebbde9dd8b8aee5ccb6e
AntonAduisky <a.mltw@yandex.ru>
2022-11-28 18:42:51 +0300

    feat: app

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import { Routes, Route } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import Container from '@mui/material/Container';
+
+import { Header } from './components';
+import { Home, FullPost, Registration, AddPost, Login } from './pages';
+import React from 'react';
+import { fetchAuthMe } from './redux/slices/auth';
+
+function App() {
+  const dispatch = useDispatch();
+
+  React.useEffect(() => {
+    dispatch(fetchAuthMe());
+  }, [dispatch]);
+
+  return (
+    <>
+      <Header />
+      <Container maxWidth="lg">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/posts/:id" element={<FullPost />} />
+          <Route path="/posts/:id/edit" element={<AddPost />} />
+          <Route path="/add-post" element={<AddPost />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Registration />} />
+        </Routes>
+      </Container>
+    </>
+  );
+}
+
+export default App;
